d5f0d301ad4ca314cd2a37cbcbe5afe42ffa15ac
vladikver2005-ops <Vladikver2005@gmail.com>
2026-04-15 17:46:11 +0300

    Initial commit

---
+++ src/main/java/com/carsh/Carsh/CarshApplication.java
@@ -0,0 +1,13 @@
+package com.carsh.Carsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carsh/Carsh/CarshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsh.Carsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
